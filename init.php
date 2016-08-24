97dc6452fb0527323a5bf5b5b366332d3cb7f45d
Ashish <ashishkarn068@gmail.com>
2016-08-24 23:15:05 +0530

    Updated INIT.PHP. Constants like mailgun public and private keys are instantiated, instances of mailgun created

--- init.php
+++ init.php
@@ -1,5 +1,6 @@
 <?php
 
+	# Database details 
 	$username = "root";
 	$password = "";
 	$database = "webapp";
@@ -13,5 +14,26 @@
 	  die('Could not connect: ' . mysqli_error());
 	}
 
+	# Setting up PHP SDK for mailgun and defining few constants
+	require_once 'vendor/autoload.php';
+
+	define('MAILGUN_KEY', 'key-9072bd52a084bd5f6254e4f4cc1f75f4');
+	define('MAILGUN_PUBKEY', 'pubkey-c41c35d8720f3f60959119e7a561749c');
+	define('MAILGUN_DOMAIN', 'sandboxb676bd5381e6447d9493afe671a8d24c.mailgun.org');
+	define('MAILGUN_LIST', 'news@sandboxb676bd5381e6447d9493afe671a8d24c.mailgun.org');
+	define('MAILGUN_SECRET', 'It\'s not who I am underneath but what I do that defines me');
+
+	# Now going to create two mailgun instances, one with MAILGUN_KEY and othe with MAILGUN_PUBKEY
+
+	# This gives all functionalities to add users, send emails and et cetera
+	$mailgun = new Mailgun\Mailgun(MAILGUN_KEY);
+
+	# This validates the email
+
+	$mailgunValidate = new Mailgun\Mailgun(MAILGUN_PUBKEY);
+
+	# This helps in adding users to lists
+
+	$mailgunOptIn = $mailgun->OptInHandler();
 
 ?>
