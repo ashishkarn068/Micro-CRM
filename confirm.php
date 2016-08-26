7c7475f213fa09127591bfa2466a845f57b6ca21
Ashish <ashishkarn068@gmail.com>
2016-08-26 19:31:21 +0530

    Echo out html code to show  Confirmation Message and Error message. (Improve UI)

--- confirm.php
+++ confirm.php
@@ -22,7 +22,31 @@ if(isset($_GET['hash']))
 
 		]);
 
-		echo 'subscribed';
+		echo'<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+				<meta name="viewport" content="width=device-width, initial-scale=1">
+					<meta name="description" content="">
+						<meta name="author" content="">
+							<title>Confirm</title>
+							<!-- Bootstrap core CSS -->
+							<link href="css/bootstrap.css" rel="stylesheet">
+								<!-- Custom styles for this template -->
+								<link href="css/signin.css" rel="stylesheet">
+								</head>
+								<body>
+									<div class="container-fluid">
+										<br />
+										
+											<h2 class="form-signin-heading" style="text-align:center">Thank you for confirming!</h2>
+											
+												</div>
+												<!-- /container -->
+											</body>
+										</html>';
+
 
 		# Sending Thank you mail 
 		$mailgun->sendMessage(MAILGUN_DOMAIN,  [
@@ -36,7 +60,30 @@ if(isset($_GET['hash']))
 	}
 	else
 	{
-		echo'error';
+		echo'<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+				<meta name="viewport" content="width=device-width, initial-scale=1">
+					<meta name="description" content="">
+						<meta name="author" content="">
+							<title>Confirm</title>
+							<!-- Bootstrap core CSS -->
+							<link href="css/bootstrap.css" rel="stylesheet">
+								<!-- Custom styles for this template -->
+								<link href="css/signin.css" rel="stylesheet">
+								</head>
+								<body>
+									<div class="container-fluid">
+										<br />
+										
+											<h2 class="form-signin-heading" style="text-align:center">Error Confirming<br /> :(</h2>
+											
+												</div>
+												<!-- /container -->
+											</body>
+										</html>';
 	}
 }
 
